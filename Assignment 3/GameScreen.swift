8925f6da60050322d9c0e032a00a2b1fa4148d10
kkumakuma <62420542+kkumakuma@users.noreply.github.com>
2020-05-31 03:23:43 +1000

    time gate breakouts can now be added anywhere/ randomised

--- Assignment 3/GameScreen.swift
+++ Assignment 3/GameScreen.swift
@@ -13,59 +13,78 @@ class GameScreen: UIViewController {
     var mainStoryArray = [""]
     var opAArray = [""]
     var opBArray = [""]
-    var timeGateArray = [4, 7, 12]
+    var timeGateArrayA = [4, 7, 12]
+    var timeGateArrayB = [3, 8, 15]
+    var alterStoryArray = [5, 14, 21]
     var i = 0
     
     @IBOutlet weak var gameTextLabel: UILabel!
     @IBOutlet weak var op1Text: UIButton!
     @IBOutlet weak var op2Text: UIButton!
+    @IBOutlet weak var op3Text: UIButton!
     
     
     @IBAction func op1Button(_ sender: UIButton) {
-        if i < mainStoryArray.count && !timeGateArray.contains(i){
-            gameTextLabel.text = mainStoryArray[i]
-            op1Text.setTitle(opAArray[i], for: [])
-            op2Text.setTitle(opBArray[i], for: [])
-            i += 1
-        } else if timeGateArray.contains(i) {
-            op1Text.isHidden = true
-            op2Text.isHidden = true
-            gameTextLabel.text = "I'm looking for something. Tell you when I find it."
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                self.op1Text.isHidden = false
-                self.op2Text.isHidden = false
+        if i < mainStoryArray.count - 1 {
+            if !timeGateArrayA.contains(i) {
+                gameTextLabel.text = mainStoryArray[i]
+                op1Text.setTitle(opAArray[i], for: [])
+                op2Text.setTitle(opBArray[i], for: [])
+                i += 1
+            } else if timeGateArrayA.contains(i) {
+                    timeBreakoutA()
+            } else if alterStoryArray.contains(i) {
+                print("wahaha")
             }
-            op1Text.setTitle(opAArray[i], for: [])
-            op2Text.setTitle(opBArray[i], for: [])
-            i += 1
         }
     }
     
-    @IBAction func op2Button(_ sender: Any) {
-        if i < mainStoryArray.count {
-            gameTextLabel.text = mainStoryArray[i]
-            op1Text.setTitle(opAArray[i], for: [])
-            op2Text.setTitle(opBArray[i], for: [])
-            i += 1
+    @IBAction func op2Button(_ sender: UIButton) {
+        if i < mainStoryArray.count - 1{
+            if !timeGateArrayB.contains(i) && !alterStoryArray.contains(i){
+                gameTextLabel.text = mainStoryArray[i]
+                op1Text.setTitle(opAArray[i], for: [])
+                op2Text.setTitle(opBArray[i], for: [])
+                i += 1
+            } else if timeGateArrayB.contains(i) {
+                timeBreakoutB()
+            } else if alterStoryArray.contains(i) {
+                print("ceebs")
+            }
         }
     }
+    
+    
+    @IBAction func op3Button(_ sender: Any) {
+        op1Text.setTitle("", for: [])
+        op1Text.isHidden = false
+        op2Text.setTitle("", for: [])
+        op2Text.isHidden = false
+        op3Text.isHidden = true
+        if gameTextLabel.text == "Can't you be a little more patient?" {
+            timeBreakoutA2()
+        } else if gameTextLabel.text == "Is it time to get up already?" {
+            timeBreakoutB2()
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initGame()
-        
-        mainStoryArray = loadTextToArray(fileName: "mainStory")!
-        print(mainStoryArray)
-        opAArray = loadTextToArray(fileName: "optionsSideA")!
-        opBArray = loadTextToArray(fileName: "optionsSideB")!
     }
     
     //opening setup
     func initGame() {
+        mainStoryArray = loadTextToArray(fileName: "mainStory")!
+        opAArray = loadTextToArray(fileName: "optionsSideA")!
+        opBArray = loadTextToArray(fileName: "optionsSideB")!
+        
         gameTextLabel.text = "Can you see me?"
         op1Text.setTitle("Yes", for: [])
         op2Text.setTitle("No", for: [])
+        op3Text.isHidden = true
     }
     
     //loads story text into arrays
@@ -79,6 +98,49 @@ class GameScreen: UIViewController {
             return content.components(separatedBy: "\n")
         } catch {
             return nil
+        }
+    }
+    
+    
+    //breakout interactions for time gated sequences on option A
+    func timeBreakoutA() {
+        op1Text.isHidden = true
+        op2Text.isHidden = true
+        gameTextLabel.text = "I'm looking for something. Brb."
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.gameTextLabel.text = "Can't you be a little more patient?"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.op3Text.isHidden = false
+            }
+        }
+        op3Text.setTitle("Did you find it?", for: [])
+    }
+    func timeBreakoutA2() {
+        gameTextLabel.text = "Yeah, I got it"
+        timeGateArrayB.remove(at: 0)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.op2Text.sendActions(for: .touchUpInside)
+        }
+    }
+    
+    //breakout interactions for time gated sequences on option B
+    func timeBreakoutB() {
+        op1Text.isHidden = true
+        op2Text.isHidden = true
+        gameTextLabel.text = "Gonna take a nap."
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.gameTextLabel.text = "Is it time to get up already?"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.op3Text.isHidden = false
+            }
+        }
+        op3Text.setTitle("Come on, wake up.", for: [])
+    }
+    func timeBreakoutB2() {
+        gameTextLabel.text = "Alright I'm up."
+        timeGateArrayB.remove(at: 0)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.op2Text.sendActions(for: .touchUpInside)
         }
     }
 }
